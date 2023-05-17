36b1c5fadfdb5b82e5d7202572cac57c95a206f7
Julia Matsievich <juliamatsievich@mail.ru>
2023-05-17 17:46:49 +0500

    refactor: function initLikeButtonEventListeners()

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ let comments = [];
 
 // Функция запроса GET
 function fetchGet() {
-		
+
 	fetch('https://webdev-hw-api.vercel.app/api/v1/julia-matsievich/comments', {
 		method: 'GET'
 	})
-	.then(response => {
-		removerLoading();
-		return response.json()
-	})
-	.then(responseData => {
+		.then(response => {
+			removerLoading();
+			return response.json()
+		})
+		.then(responseData => {
 			const appcomments = responseData.comments.map((comment) => {
 				return {
 					name: comment.author.name,
@@ -28,10 +28,10 @@ function fetchGet() {
 					likes: comment.likes,
 					isLiked: false
 				}
-			}) 
+			})
 			comments = appcomments;
 			renderComments();
-	})
+		})
 }
 
 // Функция запроса POST
@@ -40,11 +40,11 @@ function fetchPost(newComment) {
 		method: 'POST',
 		body: JSON.stringify(newComment)
 	})
-	.then(response => response.json() )
-	.then(responseData => {
+		.then(response => response.json())
+		.then(responseData => {
 			comments = responseData.comments;
 			fetchGet()
-	})
+		})
 }
 
 
@@ -115,7 +115,7 @@ function handlerAddComment() {
 			replaceAll("**/", "</div>"),
 		likes: 0,
 	}
-	
+
 	fetchPost(newComment)
 	renderLoading('<img src="./img/Reload-1.9s-197px.gif" alt="" >');
 	initialState();
@@ -127,7 +127,7 @@ function renderEditComment(element) {
 	const commentBody = parent.querySelector('.comment-body');
 	const commentText = parent.querySelector('.comment-text');
 	const divQuote = commentText.querySelector('.quote');
-	const editTextarea = commentText.innerHTML.replaceAll('<div class="quote">','/**').replaceAll('</div>','**/');
+	const editTextarea = commentText.innerHTML.replaceAll('<div class="quote">', '/**').replaceAll('</div>', '**/');
 	commentBody.innerHTML = `
 	<div class="edit-form">
 	<textarea type="textarea" class="edit-form-text" rows="4">${editTextarea}</textarea>
@@ -137,7 +137,7 @@ function renderEditComment(element) {
 	</div>
 	`
 	const editForm = parent.querySelector('.edit-form');
-	
+
 	editForm.addEventListener('click', (event) => {
 		event.stopImmediatePropagation()
 	})
@@ -154,10 +154,10 @@ function renderEditComment(element) {
 
 		const newCommentTextValue = newCommentText.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("/**", "<div class='quote'>").replaceAll("**/", "</div>");
 
-		if(divQuote) {
+		if (divQuote) {
 			commentBody.innerHTML = `<div class="comment-text">${newCommentTextValue}</div>`
 		} else {
-		commentBody.innerHTML = `<div class="comment-text">${newCommentTextValue}</div>`
+			commentBody.innerHTML = `<div class="comment-text">${newCommentTextValue}</div>`
 		}
 
 		comments[commentIndex].text = newCommentTextValue;
@@ -168,8 +168,13 @@ function renderEditComment(element) {
 function renderComments() {
 
 	const commentsHtml = comments.map((comment, index) => {
-		
-			return `
+		comment.likeButtonClass = '';
+
+		if(comment.isLiked) {
+			comment.likeButtonClass = '-active-like';
+		}
+
+		return `
 			<li class="comment" data-comment=${index}>
 			<div class="comment-header">
 			  <div>${comment.name}</div>
@@ -181,13 +186,13 @@ function renderComments() {
 			<div class="comment-footer">
 			  <div class="likes">
 				 <span class="likes-counter" data-likeCounter = "${comment.likes}">${comment.likes}</span>
-				 <button class="like-button"></button>
+				 <button class="like-button ${comment.likeButtonClass}"></button>
 				 <button class="edit-button"></button>
 				 <button class="delete-button" data-index="${index}"></button>
 			  </div>
 			</div>
 		 </li>`
-		
+
 	})
 
 	listComments.innerHTML = commentsHtml.join('');
@@ -226,24 +231,45 @@ function initEditButtonEventListeners() {
 
 //Подписка на события клика по кнопке Лайк
 function initLikeButtonEventListeners() {
+
 	const likeButtons = listComments.querySelectorAll('.like-button');
 
 	for (const likeButton of likeButtons) {
 		likeButton.addEventListener('click', (event) => {
 			event.stopPropagation();
 			const target = event.target;
-			const likesParrent = target.closest('.likes');
-			const likesCounter = likesParrent.querySelector('.likes-counter')
+			const commentParrent = target.closest('.comment');
+			const commentId = commentParrent.dataset.comment;
+			
+			comments[commentId].likes = comments[commentId].isLiked ? comments[commentId].likes -= 1 : comments[commentId].likes += 1;
+			comments[commentId].isLiked = !comments[commentId].isLiked;
+			renderComments()
 
-			if (!likeButton.matches('.-active-like')) {
-				likeButton.classList.add('-active-like');
-				likesCounter.dataset.likecounter = +likesCounter.dataset.likecounter + 1;
-				likesCounter.textContent = likesCounter.dataset.likecounter;
-			} else {
-				likeButton.classList.remove('-active-like');
-				likesCounter.dataset.likecounter = +likesCounter.dataset.likecounter - 1;
-				likesCounter.textContent = likesCounter.dataset.likecounter;
-			}
+
+			// delay(2000).then(() => {
+
+
+			// 	const target = event.target;
+			// 	const likesParrent = target.closest('.likes');
+			// 	const likesCounter = likesParrent.querySelector('.likes-counter')
+
+			// 	if (!likeButton.matches('.-active-like')) {
+			// 		likeButton.classList.add('-active-like');
+			// 		likesCounter.dataset.likecounter = +likesCounter.dataset.likecounter + 1;
+			// 		likesCounter.textContent = likesCounter.dataset.likecounter;
+			// 	} else {
+			// 		likeButton.classList.remove('-active-like');
+			// 		likesCounter.dataset.likecounter = +likesCounter.dataset.likecounter - 1;
+			// 		likesCounter.textContent = likesCounter.dataset.likecounter;
+			// 	}
+
+			// 	// comment.likes = comment.isLiked
+			// 	//   ? comment.likes - 1
+			// 	//   : comment.likes + 1;
+			// 	// comment.isLiked = !comment.isLiked;
+			// 	// comment.isLikeLoading = false;
+			// 	// renderComments();
+			//  });
 		})
 	}
 }
@@ -284,9 +310,9 @@ function initAnswerCommentEventListener() {
 // Функция для имитации запросов в API
 function delay(interval = 300) {
 	return new Promise((resolve) => {
-	  setTimeout(() => {
-		 resolve();
-	  }, interval);
+		setTimeout(() => {
+			resolve();
+		}, interval);
 	});
 }
 
